43dfbdc3a34bec530895195577ee50c5120dfebd
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-09-26 15:42:42 +0200

    functions

--- src/components/Main/MainGridElements.js
+++ src/components/Main/MainGridElements.js
@@ -4,19 +4,38 @@ import TagsElement from "./TagsElement/TagsElement";
 import Aux from "../../hoc/Aux";
 
 const mainGridElements = (props) => {
-  const items = props.data.map((item, i) => {
-    return <MainGridElementCard key={i} info={item} />;
+  const menu = props.dataTags.map((item, i) => {
+    return <TagsElement key={i} info={item} />;
   });
 
   const itemsTags = props.dataTags.map((itemTags1, i) => {
-    return <TagsElement key={i} info={itemTags1} />;
+    console.log(itemTags1.title);
+    return itemTags1.title;
   });
+
+  console.log(props.dataTags);
+  console.log(props.data);
+
+  const itemsTags2 = props.data.map((itemTags2, i) => {
+    itemTags2.tags.map((item, i) => {
+      console.log(item);
+      return item;
+    });
+    return itemTags2;
+  });
+
+  const items = props.data.map((item, i) => {
+    if (itemsTags === itemsTags2) {
+      return <MainGridElementCard key={i} info={item} />;
+    }
+  });
+  console.log(itemsTags);
+  console.log(itemsTags2);
 
   return (
     <Aux>
       <div className="main-grid">
-        <div className="tags__all">{itemsTags}</div>
-
+        <div className="tags__all">{menu}</div>
         <div className="main-grid__all">
           <div className="main-grid__row">
             <div className="main-grid__col-1-of-3">{items}</div>
