fa2754bdf7adde3b62aaa5b6a34d57ac876b9504
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-10-02 23:13:16 +0200

    styling

--- src/components/Home/HomeElement.js
+++ src/components/Home/HomeElement.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import Aux from "../../hoc/Aux";
 
-const homeElements = (props) => <div>HOME</div>;
+const homeElements = (props) => <div className="about">HOME</div>;
 //   const items = props.data.map((item, i) => {
 //     return <MainGridElementCard key={i} info={item} />;
 //   });
