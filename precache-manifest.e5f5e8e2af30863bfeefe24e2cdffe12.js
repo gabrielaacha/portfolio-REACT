6f3afbad4a9ca7d29902338458bb25132517bd75
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-10-15 20:06:09 +0200

    Updates

---
+++ precache-manifest.e5f5e8e2af30863bfeefe24e2cdffe12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3e4ad0e1ed71775a4d8a538e032cc9",
+    "url": "/gabrielaacha/portfolio-REACT/index.html"
+  },
+  {
+    "revision": "5bbb42ee20bc89c75bc8",
+    "url": "/gabrielaacha/portfolio-REACT/static/css/main.85c34215.chunk.css"
+  },
+  {
+    "revision": "9fea1d2140422536c0fb",
+    "url": "/gabrielaacha/portfolio-REACT/static/js/2.e51532e7.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/gabrielaacha/portfolio-REACT/static/js/2.e51532e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bbb42ee20bc89c75bc8",
+    "url": "/gabrielaacha/portfolio-REACT/static/js/main.3306f256.chunk.js"
+  },
+  {
+    "revision": "39759385eb9060633228",
+    "url": "/gabrielaacha/portfolio-REACT/static/js/runtime-main.5ea15959.js"
+  }
+]);
