d75424f66f2f1ce92e0a1152da6d1c1edbab8e82
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-10-28 11:42:46 +0100

    Updates

---
+++ precache-manifest.dae11655e5bc6832a4d33886e8bcb4c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc74aa1ba5a6e6b690749a8ee4a6ee1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed35fe65159c89ae65eb",
+    "url": "/static/css/main.b6909e6a.chunk.css"
+  },
+  {
+    "revision": "9fea1d2140422536c0fb",
+    "url": "/static/js/2.e51532e7.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/static/js/2.e51532e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed35fe65159c89ae65eb",
+    "url": "/static/js/main.4cfc0cdb.chunk.js"
+  },
+  {
+    "revision": "39f2e8cc378d9293797c",
+    "url": "/static/js/runtime-main.97251681.js"
+  }
+]);
